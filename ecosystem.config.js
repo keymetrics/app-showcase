58a6e56076ddb2484a2252d3e0e86e9164bb40a2
Unitech <strzelewicz.alexandre@gmail.com>
2017-02-27 17:27:28 +0100

    add dep

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,5 +2,16 @@ module.exports = {
   apps : [{
     name   : "API",
     script : "./app.js"
-  }]
+  }],
+
+  deploy : {
+    production : {
+      user : "ubuntu",
+      host : "163.172.176.141",
+      ref  : "origin/master",
+      repo : "git@github.com:keymetrics/app-showcase.git",
+      path : "/var/www/production",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env production"
+    }
+  }
 }
